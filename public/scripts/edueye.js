74afc2b5e857479fd63908f7c6a57aa142d9e16c
Einari Kurvinen <einari.kurvinen@gmail.com>
2015-09-26 13:57:04 +0300

    Rewrite almost done. For some reason remoteVideo is not playing, needs debugging

--- public/scripts/edueye.js
+++ public/scripts/edueye.js
@@ -1,4 +1,4 @@
-Happy = function(options) {
+EduEye = function(options) {
 	var self = this; 
 
 
@@ -10,33 +10,10 @@ Happy = function(options) {
 
 	$.extend(this.options, options);
 
-	this.localStream; 
-	this.remoteStream;
-
-	// this.isChannelReady = false; 
-	this.isStarted = false;
-	this.turnReady; 
-
-	this.pc; // Peer connection
-	this.pc_config = {'iceServers': 
-						[{'url': 'stun:stun.l.google.com:19302'},
-						{
-							url: 'turn:numb.viagenie.ca',
-							credential: 'muazkh',
-							username: 'webrtc@live.com'
-						},
-						{
-							url: 'turn:192.158.29.39:3478?transport=udp',
-							credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-							username: '28224511:1379330808'
-						},
-						{
-							url: 'turn:192.158.29.39:3478?transport=tcp',
-							credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-							username: '28224511:1379330808'
-						}]
-					};
-	this.pc_constraints = {'optional': [{'DtlsSrtpKeyAgreement': true}]};
+	this.constraints = { 
+		video: true,
+		audio: false
+	};
 
 	// only interested in video
 	var sdpConstraints = {
@@ -45,6 +22,21 @@ Happy = function(options) {
 			'OfferToReceiveVideo':true
 		}
 	};
+
+
+	this.localStream; 
+	this.remoteStream;
+
+	this.peerConnection; // Peer connection
+	this.peerConnectionConfig = {'iceServers': 
+						[
+							{'url': 'stun:stun.l.google.com:19302'},
+							{'url': 'stun:stun.services.mozilla.com'},
+						]
+					};
+
+
+
 
 	// for testing purposes only, room handling needs to be done in a different manner
 	this.room = this.options.defaultRoom;
@@ -82,83 +74,24 @@ Happy = function(options) {
 
 	this.socket.on('joined', function (data){
 		console.log('This peer has joined room ', data);
-		// self.isChannelReady = true;
-
-		if(self.videoDevice) {
-			console.log('>>> got video, doing the call');
-			self.doCall();
-		}
 	});
 
-	this.socket.on('message', function(message){
-		// console.log('Client received a message: ', message); 
-
-		if (message.type == 'got user media') {
-			console.log('>>>>>>>>>>>>>>> Got user media', message);
-			if(!self.isStarted) {
-				self.maybeStart();
-			}
-		}
-
-		else if (message.type === 'offer') {
-			// self.isChannelReady = true;
-			console.log('>>>>>>>>>>>>>>> Offer ', message);
-
-			if(!self.isStarted) {
-				self.maybeStart(); 
-				self.pc.setRemoteDescription(new RTCSessionDescription(message.data));
-			}
-			
-			self.doAnswer();
-
-
-		}
-
-		else if (message.type === 'answer' && self.isStarted) {
-			console.log('>>>>>>>>>>>>>>> Answer', message);
-			self.pc.setRemoteDescription(new RTCSessionDescription(message.data));
-		}
-
-		else if (message.type === 'candidate' && self.isStarted) {
-			console.log('>>>>>>>>>>>>>>> ICECandidate: ', message)
-			var candidate = new RTCIceCandidate({
-				sdpMLineIndex: message.label,
-				candidate: message.candidate
-			});
-			self.pc.addIceCandidate(candidate);
-		}
-
-		else if (message.type === 'bye' && self.isStarted) {
-			console.log('>>>>>>>>>>>>>>> Bye...', message);
-			self.handleRemoteHangup();
-		}
-		else if(message.type === 'hangup' && self.isStarted) {
-			console.log('>>>>>>>>>>>>>>> Hangup');
-			self.handleRemoteHangup();
-		}
-		else {
-			console.log(' >>>> Other message: ', message);
-		}
-	});
+	this.socket.on('message', self.gotMesasgeFromServer.bind(self));
 
 	self.init();
 
 }
 
 
-Happy.prototype.init = function() {
+EduEye.prototype.init = function() {
 	var self = this; 
-	// var constraints = {video: true, audio:false}; 
-	// getUserMedia(constraints, self.handleUserMedia.bind(self), self.handleUserMediaError.bind(self));
-
-	// var constraints = {video: true, audio:false}; 
-	// getUserMedia(constraints, 
-	// 	function(stream) {
-	// 		console.log('Asking permission');
-	// 		self.localStream = stream;
-	// 	}, 
-	// 	function(err){console.log('Error: ', err);}
-	// 	);
+	console.log("init");
+	if(self.videoDevice) {
+		getUserMedia(self.constraints, self.getUserMediaSuccess.bind(self), self.errorHandler.bind(self));
+	}
+	else {
+		console.log("not a video device");
+	}
 	
 	if (location.hostname != "localhost") {
 		self.requestTurn('https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913');
@@ -166,7 +99,7 @@ Happy.prototype.init = function() {
 }
 
 
-Happy.prototype.sendMessage = function(message) {
+EduEye.prototype.sendMessage = function(message) {
 	var self = this; 
 
 	console.log('Client sending a message: ', message); 
@@ -174,95 +107,93 @@ Happy.prototype.sendMessage = function(message) {
 	self.socket.emit('message', message);
 }
 
-Happy.prototype.handleUserMedia = function(stream) {
+EduEye.prototype.getUserMediaSuccess = function(stream) {
 	var self = this; 
-	console.log("stream", stream);
-	if(self.videoDevice === true) {
 
-		console.log("starting the stream");
+	console.log("starting the stream");
 
-		self.localStream = stream;
-		self.localVideo = $('.localVideo')[0];
+	self.localStream = stream;
+	self.localVideo = $('.localVideo')[0];
+	self.localVideo.src = window.myURL.createObjectURL(stream); 
 
-		console.log("local video", self.localVideo);
+	self.localVideo.addEventListener('canplay', function() {
+		console.log("can play, trying to start video");
+		self.localVideo.play();
+	});
 
-		self.localVideo.src = window.myURL.createObjectURL(stream); 
+	self.start(true);
+		
 
-		self.localVideo.addEventListener('canplay', function() {
-			console.log("can play, trying to start video");
-			self.localVideo.play();
-		});
+	self.sendMessage({type: 'got user media'}); 
 
-		self.sendMessage({type: 'got user media'}); 
-	}
-
-	else {
-		console.log("this is not the video device");
-	}
-	if(!self.isStarted) {
-		self.maybeStart();
-	}
+	// if(!self.isStarted) {
+	// 	self.maybeStart();
+	// }
 
 }
 
-Happy.prototype.playUserMedia = function() {
-	var self = this;
-
-	var constraints = {video: true, audio:false}; 
-	getUserMedia(constraints, self.handleUserMedia.bind(self), self.handleUserMediaError.bind(self));
-
-	console.log('Starting video and audio stream');
-	console.log('Showing local stream'); 
-	// self.localStream = stream; 
-}
-
-Happy.prototype.handleUserMediaError = function(error) {
+EduEye.prototype.errorHandler = function(error) {
 	var self = this; 
 	console.log('getUserMedia error: ', error);
 }
 
-
-Happy.prototype.maybeStart = function() {
+EduEye.prototype.start = function(isCaller) {
 	var self = this; 
 
-	// removed: && typeof self.localStream != 'undefined' and self.isChannelReady also
-	if(!self.isStarted) {
-		self.createPeerConnection(); 
+	self.peerConnection = new RTCPeerConnection(self.peerConnectionConfig); 
+	self.peerConnection.onicecandidate = self.gotIceCandidate.bind(self);
+	self.peerConnection.onaddstream = self.gotRemoteStream.bind(self);
 
-		// you don't need to send stream, if device is not available
-		if(self.localStream != 'undefined' && self.localStream != null) {
-			self.pc.addStream(self.localStream); 
+	if(self.videoDevice) {
+		console.log("added localstream");
+		self.peerConnection.addStream(self.localStream); 
+	}
+
+	if(isCaller) {
+		self.peerConnection.createOffer(self.gotDescription.bind(self), self.errorHandler.bind(self));
+	}
+}
+
+
+EduEye.prototype.gotMesasgeFromServer = function(message) {
+	var self = this; 
+
+	if(!self.peerConnection) {
+		self.start(false);
+	}
+
+	// === SDP
+	if(message.type == 'offer') {
+		self.peerConnection.setRemoteDescription(new RTCSessionDescription(message.data), function() {
+			self.peerConnection.createAnswer(self.gotDescription.bind(self), self.errorHandler.bind(self));
+		}, self.errorHandler.bind(self));
+	}
+	
+	// === ICE
+	else if (message.type === 'candidate') {
+		var candidate = new RTCIceCandidate({sdpMLineIndex: message.label, candidate: message.candidate});
+		self.peerConnection.addIceCandidate(candidate);
+	}
+
+	else if (message.type === 'bye') {
+		if(self.remoteVideo && self.remoteVideo.src) {
+			self.remoteVideo.src = null; 
+			self.remoteStream = null; 
 		}
-		self.isStarted = true;
-		console.log('OK, lets go');
+		if(self.localVideo && self.localVideo.src) {
+			self.localVideo.src = null;
+			self.localStream = null; 
+		}
 	}
-	else {
-		console.log('Something is missing...', !self.isStarted);
+
+	else if (message.type === 'got user media') {
 	}
-}
+} 
 
-
-Happy.prototype.createPeerConnection = function () {
-	var self = this; 
-
-	try {
-		self.pc = new RTCPeerConnection(null); 
-		self.pc.onicecandidate = self.handleIceCandidate.bind(self); 
-		self.pc.onaddstream = self.handleRemoteStreamAdded.bind(self); 
-		self.pc.onremovestream = self.handleRemoteStreamRemoved.bind(self); 
-
-		console.log('Created RTCPeerConnection'); 
-	} catch (e) {
-		console.log('Failed to create PeerConnection exception: ', e.message); 
-		return; 
-	}
-}
-
-Happy.prototype.handleIceCandidate = function(event) {
-	var self = this; 
-
-	console.log('handleIceCandidate event: ', event); 
-
+EduEye.prototype.gotIceCandidate = function(event) {
+	var self = this;
+	console.log("got ice");
+	
 	if(event.candidate) {
 		self.sendMessage({
 			type: 'candidate',
@@ -273,237 +204,56 @@ Happy.prototype.handleIceCandidate = function(event) {
 	}
 	else {
 		console.log('End of candidates');
+
 	}
+
 }
 
 
-Happy.prototype.handleCreateOfferError = function(event) {
-	var self = this; 
-	console.log('createOffer() error: ', e);
+EduEye.prototype.gotDescription = function(description) {
+	var self = this;
+	console.log("got description");
+	self.peerConnection.setLocalDescription(description, function() {
+		self.sendMessage({type: description.type, data: description});
+	}, function() {console.log('set description error')}); 
 }
 
-Happy.prototype.doCall = function(to) {
+EduEye.prototype.gotRemoteStream = function(event) {
 	var self = this; 
-	console.log("calling...");
-	// self.showRingingScreen(true);
+	console.log("got remoteStream");
 
-	self.playUserMedia();
-
-	if(!self.pc) {
-		self.maybeStart();
-	}
-
-	console.log('Creating offer to peer ', to); 
-	self.pc.createOffer(self.setLocalAndSendMessage.bind(self), self.handleCreateOfferError.bind(self));
-}
-
-Happy.prototype.doAnswer = function() {
-	var self = this; 
-
-	if(!self.pc) {
-		return;
-	}
-
-	// forwardTo is already set in "offer"
-	console.log('Sending answer to peer'); 
-
-	$('.qrcode').addClass('hidden'); 
-	$('.newConnection').addClass('hidden');
 	$('.remoteVideo').removeClass('hidden');
 
-	self.pc.createAnswer(self.setLocalAndSendMessage.bind(self), null, self.sdpConstraints);
+	self.remoteVideo = $('.remoteVideo')[0];
+	self.remoteVideo.src = window.myURL.createObjectURL(event.stream);
+
+	$('.newConnection').addClass('hidden');
+	$('.localVideo').addClass('hidden');
+	$('.qrcode').addClass('hidden');
+
+
+	self.remoteVideo.addEventListener('canplay', function() {
+		console.log("can play, trying to start video");
+		self.remoteVideo.play();
+	});
+
 }
 
 
-Happy.prototype.setLocalAndSendMessage = function(sessionDescription) {
-	var self = this; 
-
-	// sessionDescription.sdp = self.preferOpus(sessionDescription.sdp);
-	// sessionDescription.sdp = (sessionDescription.sdp);
-
-	self.pc.setLocalDescription(sessionDescription); 
-
-	console.log('setLocalAndSendMessage sending message ', sessionDescription); 
-	self.sendMessage({type: sessionDescription.type, data: sessionDescription});
-
-	// self.sendMessage({room: self.room, type: 'offer', data: sessionDescription});
-} 
-
-Happy.prototype.requestTurn = function(turn_url) {
+EduEye.prototype.requestTurn = function(turn_url) {
 	var self = this; 
 
 	var turnExists = false; 
-	for (var i in self.pc_config.iceServers) {
-		if(self.pc_config.iceServers[i].url.substr(0,5) === 'turn') {
+	for (var i in self.peerConnectionConfig.iceServers) {
+		if(self.peerConnectionConfig.iceServers[i].url.substr(0,5) === 'turn') {
 			turnExists = true; 
 			self.turnReady = true;
 			break; 
 		}
 	}
 
-	// if(!turnExists) {
-	// 	console.log('Getting TURN server from ', turn_url); 
-	// 	//No TURN server, get one from computeeingineondemand.appspot.com: 
-	// 	var xhr = new XMLHttpRequest();
-	// 	xhr.onreadystatechange = function() {
-	// 		if (xhr.readyState === 4 && xhr.status === 200) {
-	// 			var turnServer = JSON.parse(xhr.responseText);
-	// 			console.log('Got TURN server: ', turnServer);
-	// 			self.pc_config.iceServers.push({
-	// 				'url': 'turn:' + turnServer.username + '@' + turnServer.turn,
-	// 				'credential': turnServer.password
-	// 			});
-	// 			turnReady = true;
-	// 		}
-	// 	};
-	// 	xhr.open('GET', turn_url, true);
-	// 		xhr.send();
-	// 	}
-
 }
-
-
-Happy.prototype.handleRemoteStreamAdded = function(event) {
-	var self = this; 
-
-	console.log('Remote stream added', event.stream);
-	self.remoteVideo = $('.remoteVideo')[0]
-	self.remoteVideo.src = window.myURL.createObjectURL(event.stream); 
-	self.remoteStream = event.stream; 
-
-	self.remoteVideo.addEventListener('canplay', function() {
-		self.remoteVideo.play();
-	});
-}
-
-Happy.prototype.handleRemoteStreamRemoved = function(event) {
-	var self = this; 
-	console.log('Remote stream removed. Event: ', event); 
-}
-
-Happy.prototype.hangup = function() {
-	var self = this; 
-
-
-	self.localVideo = $('.localVideo')[0];
-	self.remoteVideo = $('.remoteVideo')[0];
-
-  	if(self.localVideo && self.videoDevice === true) {
-	  	self.localVideo.pause();
-	  	self.localVideo.src = null;
-  	}
-  	if(self.remoteVideo) {
-	  	self.remoteVideo.pause();
-	  	self.remoteVideo.src = null; 
-  	}
-
-  	// self.localStream = null;
-
-	console.log('Hanging up'); 
-	self.stop(); 
-	// self.sendMessage({room: self.room, type: 'bye'});
-}
-
-Happy.prototype.handleRemoteHangup = function() {
-	var self = this; 
-	console.log('Remote hangup');
-	self.hangup();
-}
-
-Happy.prototype.stop = function() {
-	var self = this; 
-
-	self.isStarted = false; 
-	if(self.pc) {
-		self.pc.close(); 
-		self.pc = null; 
-	}
-
-	self.sendMessage({type: 'hangup'});
-
-}
-
-
-///////////////////////////////////////////////////////////////////////////
-// Set Opus as the default audio codec if it's present.
-Happy.prototype.preferOpus = function(sdp) {
-	var self = this;
-  var sdpLines = sdp.split('\r\n');
-  var mLineIndex;
-  // Search for m line.
-  for (var i = 0; i < sdpLines.length; i++) {
-      if (sdpLines[i].search('m=audio') !== -1) {
-        mLineIndex = i;
-        break;
-      }
-  }
-  if (mLineIndex === null) {
-    return sdp;
-  }
-
-  // If Opus is available, set it as the default in m line.
-  for (i = 0; i < sdpLines.length; i++) {
-    if (sdpLines[i].search('opus/48000') !== -1) {
-      var opusPayload = self.extractSdp(sdpLines[i], /:(\d+) opus\/48000/i);
-      if (opusPayload) {
-        sdpLines[mLineIndex] = self.setDefaultCodec(sdpLines[mLineIndex], opusPayload);
-      }
-      break;
-    }
-  }
-
-  // Remove CN in m line and sdp.
-  sdpLines = self.removeCN(sdpLines, mLineIndex);
-
-  sdp = sdpLines.join('\r\n');
-  return sdp;
-}
-
-Happy.prototype.extractSdp = function(sdpLine, pattern) {
-  var result = sdpLine.match(pattern);
-  return result && result.length === 2 ? result[1] : null;
-}
-
-// Set the selected codec to the first in m line.
-Happy.prototype.setDefaultCodec = function(mLine, payload) {
-	var self = this;
-  var elements = mLine.split(' ');
-  var newLine = [];
-  var index = 0;
-  for (var i = 0; i < elements.length; i++) {
-    if (index === 3) { // Format of media starts from the fourth.
-      newLine[index++] = payload; // Put target payload to the first.
-    }
-    if (elements[i] !== payload) {
-      newLine[index++] = elements[i];
-    }
-  }
-  return newLine.join(' ');
-}
-
-// Strip CN from sdp before CN constraints is ready.
-Happy.prototype.removeCN = function(sdpLines, mLineIndex) {
-  var mLineElements = sdpLines[mLineIndex].split(' ');
-  // Scan from end for the convenience of removing an item.
-  for (var i = sdpLines.length-1; i >= 0; i--) {
-    var payload = self.extractSdp(sdpLines[i], /a=rtpmap:(\d+) CN\/\d+/i);
-    if (payload) {
-      var cnPos = mLineElements.indexOf(payload);
-      if (cnPos !== -1) {
-        // Remove CN payload from m line.
-        mLineElements.splice(cnPos, 1);
-      }
-      // Remove CN line in sdp
-      sdpLines.splice(i, 1);
-    }
-  }
-
-  sdpLines[mLineIndex] = mLineElements.join(' ');
-  return sdpLines;
-}
-
-
-
-Happy.prototype.trace = function(text, obj) {
+ 
+EduEye.prototype.trace = function(text, obj) {
 	console.log((performance.now() / 1000).toFixed(3) + ": " + text, obj);
 }
